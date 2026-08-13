2dd8cbe2031648b555265f49772089eb3cb009d7
Kunal Kumar <kunalkumar88827@gmail.com>
2026-08-13 17:36:02 +0530

    sitemap and robots add

---
+++ app/tools/student/page.tsx
@@ -0,0 +1,9 @@
+export default function FinancePage() {
+  return (
+    <div className="max-w-6xl mx-auto p-10">
+      <h1 className="text-4xl font-bold">
+        Finance Tools
+      </h1>
+    </div>
+  );
+}
